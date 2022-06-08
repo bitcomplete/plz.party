37229dec5b07b9c515eb6e47402d4a66b90875dd
Mikael Gramont <mgramont@gmail.com>
2022-06-08 12:36:40 -0400

    Broken popup code

--- popup.js
+++ popup.js
@@ -1,24 +1,22 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+const copyEl = document.getElementById('copy');
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+(async function asyncFunction() {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: copyId,
   });
-});
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+  // The body of this function will be executed as a content script inside the
+  // current page
+  function copyId() {
+    copyEl.addEventListener('click', function listener(e) {
+      e.preventDefault();
+      const params = new URLSearchParams(window.location.search);
+      params.set('userId', document.getElementById("userId").value);
+      const inviteUrl = `${window.location.origin}${params.toString()}`;
+      window.alert({inviteUrl});
+      console.log({inviteUrl});
+    })
+  }
+})();
